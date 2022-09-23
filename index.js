29fb5d1ab67f57235dabb6cec6ae2e8e89457d2e
Dan Nguyen <xdanielnguyen@gmail.com>
2022-09-23 15:02:09 -0600

    testing onclick

--- index.js
+++ index.js
@@ -13,12 +13,6 @@ submitForm.addEventListener("submit", (e) => {
     inputTextBox.value = ""
 })
 
-listContainer.addEventListener("click", e => {
-    if(e.target.parentElement.id) {
-        console.log(e.target.parentElement);
-    }
-})
-
 function displayTasks() {
     fetch("http://localhost:3000/todos")
     .then(res => res.json())
@@ -29,7 +23,7 @@ function displayTasks() {
                 `
                 <article class="todo__list__task">
                     <p class="todo__list__title task--completed">${element.title}</p>
-                    <button class="btn edit-button" id="edit-${element.id}"><svg class="icon" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="EditIcon" aria-label="fontSize small"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path></svg></button>
+                    <button class="btn edit-button" id="edit-${element.id}" onclick="() => console.log("clicked")"><svg class="icon" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="EditIcon" aria-label="fontSize small"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path></svg></button>
                     <button class="btn delete-button" id="delete-${element.id}"><svg class="icon" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="DeleteIcon" aria-label="fontSize small"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path></svg></button>
                 </article>
                 `
@@ -47,6 +41,7 @@ function displayTasks() {
         })
     })
 }
+
 
 function handleSubmit(task) {
     fetch("http://localhost:3000/todos", {
